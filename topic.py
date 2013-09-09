b2bf2d614cb430afb1c47e8bc31ade1497c63d88
Matthew Ernisse <mernisse@ub3rgeek.net>
2013-09-08 23:49:38 -0400

    some info was leaky here, ship that off to private.py and clean up some docs

--- topic.py
+++ topic.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python -tt
+''' topic.py (c) 2013 Matthew J Ernisse <mernisse@ub3rgeek.net>
 
+Take a custom calendar(1) file from a remote server and set a topic
+on the channel from the notable events of the day.
+
+This tries to be polite, if the topic is set by someone other than the bot
+or if the topic doesn't look like an event it won't change the topic.
+
+'''
 import core
 import random
 import re
@@ -13,34 +21,34 @@ from botlogger import *
 EVENTS = {}
 
 def load_calendar():
-	''' fetch the calendar from iorek via sftp '''
+	''' fetch the calendar from a remote server via sftp '''
 	global EVENTS
 
 	remote = { 
-		'hostname': 'iorek.ice-nine.org',
-		'location': '/usr/share/calendar/calendar.iorek',
+		'username': private.SFTP_USERNAME,
+		'password': private.SFTP_PASSWORD,
+		'hostname': private.SFTP_HOSTNAME,
+		'location': private.SFTP_LOCATION,
 	}
 
-#	try:
-#		username = private.IOREK_USERNAME
-#		password = private.IOREK_PASSWORD
-#
-#		transport = paramiko.Transport((remote['hostname'], 22))
-#		transport.connect(
-#			username = username,
-#			password = password)
-#
-#		sftp = paramiko.SFTPClient.from_transport(transport)
-#		sftp.get(remote['location'], 'calendar.iorek')
-#		sftp.close()
-#		transport.close()
-#	except Exception, e:
-#		err('topic - failed to get calendar: %s' % str(e))
-#		return
+	try:
+
+		transport = paramiko.Transport((remote['hostname'], 22))
+		transport.connect(
+			username = remote['username'],
+			password = remote['password'])
+
+		sftp = paramiko.SFTPClient.from_transport(transport)
+		sftp.get(remote['location'], private.SFTP_LOCAL)
+		sftp.close()
+		transport.close()
+	except Exception, e:
+		err('topic - failed to get calendar: %s' % str(e))
+		return
 
 	try:
 		count = 0
-		fd = open('calendar.iorek')
+		fd = open(private.SFTP_LOCAL)
 		for line in fd:
 			matches = re.search(r'^(\d+)/(\d+)\s+(.*)', line, re.I)
 			if not matches:
@@ -67,6 +75,10 @@ def load_calendar():
 	log('topic - loaded %i events for %i days' % (count, days))
 
 def emit_event(month, day):
+	''' return an event string, if nothing is in the calendar file for
+	today, emit a default string.
+
+	'''
 	global EVENTS
 	mmdd = "%s-%s" % (month, day)
 
@@ -79,14 +91,13 @@ def emit_event(month, day):
 		EVENTS[mmdd][random.randint(0, len(EVENTS[mmdd]) -1)]
 	)
 
-def periodic(self):
-	for channel in self.channels:	
-		self.topic(channel)
-
 def topicUpdated(self, user, channel, topic):
-	#
-	# if someone else set the topic, don't override it
-	#
+	''' topicUpdated() gets called by the default irc_RPL_TOPIC()
+	so catch that.
+
+	Be polite.
+
+	'''
 	if not user == self.nickname and topic:
 		return
 
@@ -94,6 +105,9 @@ def topicUpdated(self, user, channel, topic):
 	thisday = time.strftime('%d')
 
 	if not topic:
+		# if the topic is unset, that's a signal we can take it over.
+		# this is what happens if the channel is new / never had a topic
+	 	# (the user will be set to the server name).
 		self.topic(channel, emit_event(thismonth, thisday))
 		return
 
@@ -108,6 +122,11 @@ def topicUpdated(self, user, channel, topic):
 		return
 
 	self.topic(channel, emit_event(thismonth, thisday))
+
+def periodic(self):
+	''' get all the topics for all the channels we are joined to. '''
+	for channel in self.channels:	
+		self.topic(channel)
 
 load_calendar()
 core.register_module(__name__)
