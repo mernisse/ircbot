bf1e2b0423423e60eef0a33751bbd365627b5ad4
Matthew Ernisse <mernisse@ub3rgeek.net>
2013-09-09 23:20:33 -0400

    some TLC for uberurls, less chattiness, better docs, better errors, some style cleanup, canonicalize youtube urls

--- uberurls.py
+++ uberurls.py
@@ -26,27 +26,62 @@ SQL_PASS = private.UBERURLS_SQL_PASS
 SQL_DB = private.UBERURLS_SQL_DB
 
 def processurl(speaker, url):
+	''' try to fetch a url from the interwebs.  store some info about
+	who mentioned it in the database (keeping track of mention counts)
+	along with some meta information from the url (content of the html
+	title tag if it is present or the mime-type if it is not a text/html
+	page).
+
+	''' 
 	log('processing url %s from %s' % (speaker, url))
 	try:
 		request = urllib2.Request(url)
 		request.add_header('User-Agent', 'uberurls ircbot/python')
 		fp = urllib2.urlopen(request)
+
 	except urllib2.URLError, e:
-		log("uberurls - could not open %s, %s" % (url, e.reason))
+		err("uberurls - could not urlopen %s, %s" % (url, e.reason))
 		return "%s is dead %s" % (url, e.reason)
 
 	#
 	# Clean up urls here
 	#
 
+	# sanitize youtube links.
+	if re.search(r'youtube\.com|youtu\.be', url):
+		parsed = urlparse.urlparse(url)
+		query = urlparse.parse_qs(parsed.query)
+		canonical_qs = ''
+		canonical_url = ''
+		if 't' in query:
+			canonical_qs += '&t=' + str(query['t'][0])
+
+		if 'list' in query:
+			canonical_qs += '&list=' + str(query['list'][0])
+
+		if 'v' in query:
+			canonical_url += 'https://youtube.com/watch?v=' + str(query['v'][0])
+			canonical_url += canonical_qs
+
+		elif 'youtu.be' in parsed.netloc:
+			canonical_url += 'https://youtube.com/watch?v=' + str(parsed.path[1:])
+			canonical_url += canonical_qs
+
+		else:
+			canonical_url = url
+			canonical_url = re.sub('^http', 'https', url)
+
+		url = canonical_url
+
 	try:
 		sql = MySQLdb.connect(SQL_HOST, SQL_USER, SQL_PASS, SQL_DB)
 		cursor = sql.cursor()
 		cursor.execute("SELECT * FROM urls WHERE url = %s", (url))
 		row = cursor.fetchone()
+
 	except Exception, e:
-		log('uberurls - db error: %s' % str(e))
-		return '[uberurls - DB ERROR]'
+		err('uberurls - SELECT error: %s' % str(e))
+		return
 
 	if not row:
 		shorturl = fetchshorturl(url)		
@@ -61,9 +96,10 @@ def processurl(speaker, url):
 				(url, shorturl, original, count)
 			)
 			sql.commit()
+		
 		except Exception, e:
-			log('uberurls - db error on INSERT: %s' % str(e))
-			return '[uberurls - DB ERROR]'
+			err('uberurls - INSERT error: %s' % str(e))
+			return
 
 	else:
 		shorturl = row[2]
@@ -76,9 +112,10 @@ def processurl(speaker, url):
 				(count, url)
 			)
 			sql.commit()
+
 		except Exception, e:
-			log('could not update URL in DB: %s' % str(e))
-			return '[uberurls - DB ERROR]'
+			err('uberurls - UPDATE error: %s' % str(e))
+			return
 
 		#
 		# fetchshorturl() will return the original row if it cannot
@@ -92,44 +129,38 @@ def processurl(speaker, url):
 					(shorturl,))
 				sql.commit()
 				sql.close()
-			except Exception, e:
-				log('could not reshorten URL: %s' % str(e))
-				return '[uberurls - DB ERROR]'
 
+			except Exception, e:
+				err('uberurls - could not reshorten URL: %s' % str(e))
+				return
 	#
 	# handle embedded encoding in the content-type field
 	#
 	if not fp.info()['content-type'].startswith('text/html'):
 		return "%s [%s, %s mentioned %ix]" % (
-			shorturl,
-			fp.info()['content-type'],
-			original,
-			count
+			shorturl, fp.info()['content-type'], original, count
 		)
 
 	try:
 		soup = BeautifulSoup(fp)
 		title = cgi.escape(soup.title.string)
 		title = title.encode('ascii', 'xmlcharrefreplace')
+
 	except Exception, e:
-		log("uberurls - error getting title %s" % str(e))
+		err("uberurls - error getting title %s" % str(e))
 		return "%s [%s mentioned %ix]" % (
-			shorturl,
-			original,
-			count
+			shorturl, original, count
 		)
 
 	return "%s [%s, %s mentioned %ix]" % (
-		shorturl,
-		title,
-		original,
-		count
+		shorturl, title, original, count
 	)
 
 def fetchshorturl(url):
-	#
-	# Do not shorten already shortened urls.
-	#
+	''' shorten the given url using the super excellent uber.hk
+	url shortening service!
+
+	'''
 	parsed = urlparse.urlparse(url)
 	if parsed.netloc.startswith('uber.hk'):
 		return url
@@ -138,10 +169,10 @@ def fetchshorturl(url):
 		qurl = urllib.quote(url)
 		fp = urllib2.urlopen("http://uber.hk/api/add?u=%s" % (qurl))
 		return "".join(fp.readlines()).strip()
+
 	except Exception, e:
-		log('uberurls - failed to shorten %s, %s' % (
-			url,
-			str(e)
+		err('uberurls - failed to shorten %s, %s' % (
+			url, str(e)
 		))
 		return url
 
@@ -159,7 +190,6 @@ def privmsg(self, user, channel, msg):
 		for url in matches:
 			msg = processurl(speaker, url)
 			if not msg:
-				err('error doing url stuff')
 				return
 
 			self.msg(channel, msg, only=True)
@@ -168,8 +198,6 @@ def privmsg(self, user, channel, msg):
 		raise
 
 	except Exception, e:
-		err('uberurls - ERROR: %s' % (
-			str(e)
-		))
+		err('uberurls - Uncaught Error: %s' % (str(e)))
 
 core.register_module(__name__)
